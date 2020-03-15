4ef6be304b932bc930bb73109f10244bfb508a9f
N1k0lay78 <kolya.toropof@gmail.com>
2020-03-15 21:07:53 +0300

    сделана тренировка

--- #2.py
+++ #2.py
@@ -8,6 +8,15 @@ def index(title):
     return render_template('base.html', style=url_for('static', filename='css/style.css'), title=title)
 
 
+@app.route('/treaning/<specialist>')
+def trening(specialist):
+    if 'инженер' in specialist or 'строитель' in specialist:
+        file = 'inginer'
+    else:
+        file = 'gumunit'
+    return render_template('tren.html', filename=url_for('static', filename=f'image/{file}.png'), style=url_for('static', filename='css/style.css'), title="Тренировка")
+
+
 if __name__ == '__main__':
-    print('http://127.0.0.1:8080/index')
+    print('http://127.0.0.1:8080/treaning/')
     app.run(port=8080, host='127.0.0.1')
