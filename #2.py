7fc38a737edc0380d7719f79099091ab71dfea95
N1k0lay78 <kolya.toropof@gmail.com>
2020-03-15 21:36:17 +0300

    сделан список професий

--- #2.py
+++ #2.py
@@ -14,9 +14,18 @@ def trening(specialist):
         file = 'inginer'
     else:
         file = 'gumunit'
-    return render_template('tren.html', filename=url_for('static', filename=f'image/{file}.png'), style=url_for('static', filename='css/style.css'), title="Тренировка")
+    return render_template('tren.html', filename=url_for('static', filename=f'image/{file}.png'),
+                           style=url_for('static', filename='css/style.css'), title="Тренировка")
+
+
+@app.route('/list_prof/<type>')
+def list_prof(type):
+    profs = ['инженер', 'пилот', 'строитель', 'врач', 'климатолог', 'астогеолог', 'гляциолог', 'метеоролог',
+             'киберинженер', 'штурман']
+    return render_template('list-prof.html', type=type, style=url_for('static', filename='css/style.css'),
+                           title="Cписок профессий", profs=profs)
 
 
 if __name__ == '__main__':
-    print('http://127.0.0.1:8080/treaning/')
+    print('http://127.0.0.1:8080/list_prof/ul')
     app.run(port=8080, host='127.0.0.1')
